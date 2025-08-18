5cc507ba2addaa7a077c42ce188ebe0bb116288c
ayaxFE <thefoxde2005@gmail.com>
2025-08-18 15:47:15 -0300

    Update main.java

--- main.java
+++ main.java
@@ -1,6 +1,11 @@
 
 public class main {
     public static void main(String[] args) {
+<<<<<<< Updated upstream
         System.out.println("Hola, esto es el Repositorio");
+=======
+        System.out.println("DSAPKJDASJKLÑADSJKLÑJKLÑDAS");gfhfghfghfhfghfhfghfgh
+        System.out.println("holaa perri");
+>>>>>>> Stashed changes
     }
 }
